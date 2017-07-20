93f51270750e1fcab6de37ab9dce559e90f11823
Seunghun Lee <waydi1@gmail.com>
2017-07-20 23:51:53 +0900

    Add Todo model

---
+++ RxSimpleTodo/Model/Todo.swift
@@ -0,0 +1,17 @@
+//
+//  File.swift
+//  RxSimpleTodo
+//
+//  Created by Seunghun Lee on 2017-07-20.
+//  Copyright © 2017 Seunghun Lee. All rights reserved.
+//
+
+import Foundation
+
+class Todo {
+  var content: String
+
+  init(content: String) {
+    self.content = content
+  }
+}

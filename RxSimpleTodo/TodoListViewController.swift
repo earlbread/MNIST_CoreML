ac767085277e1bd70caf6a0e0d73113a34be82cd
Seunghun Lee <waydi1@gmail.com>
2017-08-19 23:15:04 +0900

    Remove table view datasource and delegate

--- RxSimpleTodo/TodoListViewController.swift
+++ RxSimpleTodo/TodoListViewController.swift
@@ -17,27 +17,6 @@ class TodoListViewController: UIViewController {
 
   override func viewDidLoad() {
     super.viewDidLoad()
-
-    tableView.dataSource = self
-    tableView.delegate = self
   }
 
-}
-
-extension TodoListViewController: UITableViewDataSource, UITableViewDelegate {
-  func numberOfSections(in tableView: UITableView) -> Int {
-    return 1
-  }
-
-  func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-    return 1
-  }
-
-  func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-    let cell = tableView.dequeueReusableCell(withIdentifier: "TodoCell", for: indexPath)
-
-    cell.textLabel?.text = "HI"
-
-    return cell
-  }
 }

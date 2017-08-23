aad9e6d869dfc9985eb3e311f926a56724fd7c35
Seunghun Lee <waydi1@gmail.com>
2017-08-23 22:01:16 +0900

    Connect todo item with table view

--- RxSimpleTodo/TodoListViewController.swift
+++ RxSimpleTodo/TodoListViewController.swift
@@ -7,6 +7,9 @@
 //
 
 import UIKit
+import RxSwift
+import RxCocoa
+import RxDataSources
 
 class TodoCell: UITableViewCell {
   @IBOutlet weak var contentLabel: UILabel!
@@ -14,10 +17,19 @@ class TodoCell: UITableViewCell {
 
 class TodoListViewController: UIViewController {
   @IBOutlet weak var tableView: UITableView!
-  var items = [Todo]()
+  var items = Variable([Todo]())
+  let disposeBag = DisposeBag()
 
   override func viewDidLoad() {
     super.viewDidLoad()
-  }
 
+    items.value.append(Todo(content: "HI"))
+
+    items.asObservable()
+      .bind(to: tableView.rx.items(cellIdentifier: "TodoCell",
+                                   cellType: TodoCell.self)) { (_, element, cell) in
+        cell.contentLabel?.text = element.content
+      }
+      .disposed(by: disposeBag)
+  }
 }

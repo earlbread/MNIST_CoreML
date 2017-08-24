d3191d981ef3a83aec4550a27ae38954b59d7c29
Seunghun Lee <waydi1@gmail.com>
2017-08-24 22:26:09 +0900

    Remove Project

---
+++ MNIST_CoreML/MNIST_CoreML/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MNIST_CoreML
+//
+//  Created by Seunghun Lee on 2017-08-24.
+//  Copyright © 2017 Seunghun Lee. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
--- MNIST_CoreML/MNIST_CoreMLTests/MNIST_CoreMLTests.swift
+++ MNIST_CoreML/MNIST_CoreMLTests/MNIST_CoreMLTests.swift
@@ -1,36 +1,36 @@
 //
-//  RxSimpleTodoTests.swift
-//  RxSimpleTodoTests
+//  MNIST_CoreMLTests.swift
+//  MNIST_CoreMLTests
 //
-//  Created by Seunghun Lee on 2017-07-17.
+//  Created by Seunghun Lee on 2017-08-24.
 //  Copyright © 2017 Seunghun Lee. All rights reserved.
 //
 
 import XCTest
-@testable import RxSimpleTodo
+@testable import MNIST_CoreML
 
-class RxSimpleTodoTests: XCTestCase {
-
+class MNIST_CoreMLTests: XCTestCase {
+    
     override func setUp() {
         super.setUp()
         // Put setup code here. This method is called before the invocation of each test method in the class.
     }
-
+    
     override func tearDown() {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
         super.tearDown()
     }
-
+    
     func testExample() {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
     }
-
+    
     func testPerformanceExample() {
         // This is an example of a performance test case.
         self.measure {
             // Put the code you want to measure the time of here.
         }
     }
-
+    
 }

6978d2e75492874590b6b4beb524dfa3cbe20d7c
Seunghun Lee <waydi1@gmail.com>
2017-08-27 00:25:11 +0900

    Add prediction and confidence labels

--- MNIST_CoreML/MNIST_CoreML/ViewController.swift
+++ MNIST_CoreML/MNIST_CoreML/ViewController.swift
@@ -12,19 +12,33 @@ import CoreML
 
 class ViewController: UIViewController {
   @IBOutlet weak var drawView: DrawView!
-
+  @IBOutlet weak var predictionLabel: UILabel!
+  @IBOutlet weak var confidenceLabel: UILabel!
+  
   override func viewDidLoad() {
     super.viewDidLoad()
+
+    hiddenLabels()
   }
 
   @IBAction func resetCanvas(_ sender: UIButton) {
     drawView.lines = []
     drawView.setNeedsDisplay()
+    hiddenLabels()
   }
 
   @IBAction func predictNumber(_ sender: UIButton) {
+    showLabels()
   }
 
+  private func hiddenLabels() {
+    predictionLabel.isHidden = true
+    confidenceLabel.isHidden = true
+  }
 
+  private func showLabels() {
+    predictionLabel.isHidden = false
+    confidenceLabel.isHidden = false
+  }
 }
 
